66f8ec0529ff3aeec0cf90d6db600ed83e6ed0c1
Ubaidulloh02 <ooresab@gmail.com>
2024-01-16 14:13:18 +0600

    change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import './App.css'
-import Header from './components/Header'
 function App() {
-	return (
-		<div className='App'>
-			<Header />
-		</div>
-	)
+	return <div className='App'></div>
 }
 
 export default App
