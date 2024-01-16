6317ebd4eb7a4399e173a41c972809762c616628
rano <rahmanalievarano89@gmail.com>
2024-01-16 13:01:02 +0600

    Input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import SekerInput from './components/sekerInput';
 
 function App() {
   return (
     <div className="App">
       <h1>Avtandil</h1>
+      <SekerInput/>
     </div>
   );
 }
---
+++ src/components/SekerInput.jsx
@@ -0,0 +1,24 @@
+ import React from 'react'
+import styled from 'styled-components'
+ 
+ const SekerInput = () => {
+   return (
+    <div>
+        <Container1> 
+        <section>
+            <option value="">Производитель</option>
+            <input type="text" />
+        </section>
+        <input type="text" />
+        </Container1>
+        <div>
+            <input type="text" />
+            <input type="text" />
+        </div>
+    </div>
+  
+  )
+}
+ export default SekerInput
+ 
+ 
