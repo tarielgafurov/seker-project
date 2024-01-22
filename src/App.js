d917c2502546cc4c3dd81cf91cb850876a59913b
rano <rahmanalievarano89@gmail.com>
2024-01-22 15:46:08 +0600

    users

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import './App.css';
- import InputSeker from './components/InputSeker';
-import Users from './components/Users';
 
 function App() {
   return (
     <div className="App">
-     {/* <InputSeker/> */}
-     {/* <SliderMaterial/>  */}
-     <Users/>
     </div>
   );
 }
